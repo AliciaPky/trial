1e6d90aea157211cb895cb49fece7286780b4e9a
lunnybun <aliciapun25@gmail.com>
2024-12-13 15:04:35 +0800

    Set up JavaFX with FXML

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,26 @@
 package com.example;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/MainView.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("JavaFX and FXML Example");
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub!");
+        launch(args);
     }
 }
---
+++ src/main/java/src/main/resources/com/example/MainController.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+
+public class MainController {
+    public void handleButtonClick(ActionEvent event) {
+        System.out.println("Button clicked!");
+    }
+}
